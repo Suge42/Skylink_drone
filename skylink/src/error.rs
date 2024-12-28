b639921d6ff1a789bd4a44c856594fe37023db82
Suge42 <lsandrini04@gmail.com>
2024-12-28 21:46:50 +0100

    Update to create_error

--- skylink/src/error.rs
+++ skylink/src/error.rs
@@ -6,20 +6,23 @@ pub fn create_error(starting_id: NodeId, packet: Packet, nack_type: NackType) ->
     if let PacketType::MsgFragment(msg_fragment) = packet.pack_type {
         fragment_index = msg_fragment.fragment_index;
     }
-    let position = packet.routing_header.hops
+
+    let hops = packet.routing_header.hops
+        .into_iter()
+        .rev()
+        .collect::<Vec<NodeId>>();
+    let position = hops
         .iter()
-        .position(|x| *x == starting_id).unwrap();
+        .position(|x| *x == starting_id)
+        .unwrap();
     Packet {
-        pack_type: PacketType::Nack(Nack{
+        pack_type: PacketType::Nack(Nack {
             fragment_index,
             nack_type,
         }),
-        routing_header: SourceRoutingHeader{
-            hop_index: 0,
-            hops: packet.routing_header.hops[0..position + 1].to_vec()
-                .into_iter()
-                .rev()
-                .collect::<Vec<NodeId>>()
+        routing_header: SourceRoutingHeader {
+            hop_index: position,
+            hops
         },
         session_id: packet.session_id,
     }
